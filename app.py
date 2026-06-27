10284cd335b3fda15b501826c3e04fd4a28c9e9c
ayushupadhyay456 <ayushupadhyay456@gmail.com>
2026-06-27 22:00:50 +0530

    socket

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='gevent')
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
